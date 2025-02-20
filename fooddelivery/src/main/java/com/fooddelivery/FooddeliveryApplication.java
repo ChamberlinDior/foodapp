967d31b6b0182a8b74214efa72281bbc1fb3b9bc
ChamberlinDior <chamberlindior@gmail.com>
2025-02-20 00:36:18 -0500

    ddd

--- fooddelivery/src/main/java/com/fooddelivery/FooddeliveryApplication.java
+++ fooddelivery/src/main/java/com/fooddelivery/FooddeliveryApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,15 +25,20 @@ public class FooddeliveryApplication implements CommandLineRunner {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000", "http://51.210.183.120")
+						.allowedOrigins("http://localhost:3000", "http://98.81.244.176")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");
 			}
-
-
 		};
+	}
+
+	@RestController
+	public class DefaultController {
+		@GetMapping("/")
+		public String home() {
+			return "Bienvenue sur FoodDelivery ! Accédez à notre application à l'adresse : http://98.81.244.176:8080";
+		}
 	}
 }
